1acbdefaa2b4502c3f8676fd1ce1573bbe159673
NicoCI <ibisshagrat@hotmail.com>
2020-12-18 08:26:23 +0100

    Cambios menores (identacion y formato), y problemas sincronización con gitHub

--- src/com/forum/Application/Main.java
+++ src/com/forum/Application/Main.java
@@ -6,49 +6,39 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import es.lanyu.forum.Comentario;
-import es.lanyu.forum.Imprimible;
-//import es.lanyu.forum.Recomendacion;
-import es.lanyu.forum.Tema;
-import es.lanyu.forum.Usuario;
-import es.lanyu.forum.UsuarioExterno;
-import es.lanyu.forum.UsuarioImpl;
+import es.lanyu.forum.*;
 import es.lanyu.forum.test.DatosPrueba;
 
-import com.github.likes.Like;
-import com.github.likes.Likes;
+import com.github.likes.*;
 
 public class Main {
+	// Se crea como list para no tener que parsear o copiar a otra lista a la hora de ordenar
+	static List<Comentario> comentarios = new ArrayList<Comentario>(DatosPrueba.COMENTARIOS); 
+	static final String MENSAJE_INSERCION_OK = "El comentario se annadio correctamente";
+	static final String MENSAJE_INSERCION_ERROR = "El comentario no se pudo annadir";
 
-	static List<Comentario> comentarios = new ArrayList<Comentario>(DatosPrueba.COMENTARIOS);  //Se crea como list para no tener que parsear o copiar a otra lista a la hora de ordenar
-	static final String MENSAJE_INSERCION_OK = "El comentario se añadió correctamente";
-	static final String MENSAJE_INSERCION_ERROR = "El comentario no se pudo añadir";
-
-	static List<Usuario> usuarios = new ArrayList();
+	static List<Usuario> usuarios = new ArrayList<Usuario>();
 
 	public static void main(String[] args) {
 
 		Tema tema1 = new Tema("Tema 1");
-		
+
 		Usuario usuario3 = new UsuarioImpl("user3");
 		usuarios.add(usuario3);
 		usuarios.add(comentarios.get(0).getUsuario());
 		usuarios.add(comentarios.get(1).getUsuario());
-		
-		comentar(usuario3, tema1, "Comentario añadido");
-	
+
+		comentar(usuario3, tema1, "Comentario annadido");
+
 		Usuario usuarioExterno = new UsuarioExterno("usuarioExterno");
 		comentar(usuarioExterno, tema1, "Soy un usuario externo");
-		
+
 		mostrarComentariosOrdenados();
-		
+
 		Likes.recomendar(comentarios.get(1), usuario3.toString());
 
-//		getUsuariosRecomendacion(comentarios.get(0));
-
-		
 		Comparator<Comentario> compararPorRecomendaciones = new Comparator<>() {
-		
+
 			public int compare(Comentario obj1, Comentario obj2) {
 				int resultado = 0;
 				if (((Integer) Comentario.getLikes(obj2)).compareTo((Integer) Comentario.getLikes(obj1)) == 0) {
@@ -57,10 +47,10 @@ public class Main {
 					resultado = ((Integer) Comentario.getLikes(obj2)).compareTo((Integer) Comentario.getLikes(obj1));
 				}
 				return resultado;
-				
+
 			}
 		};
-		
+
 		Likes.recomendar(comentarios.get(2), usuarios.get(2).getUser());
 		Likes.recomendar(comentarios.get(2), usuarios.get(2).getUser());
 		Likes.recomendar(comentarios.get(2), usuarios.get(2).getUser());
@@ -68,17 +58,12 @@ public class Main {
 		Likes.recomendar(comentarios.get(0), usuarios.get(2).getUser());
 		Likes.recomendar(comentarios.get(0), usuarios.get(2).getUser());
 
-
-
-
 		Collections.sort(comentarios, compararPorRecomendaciones);
 		System.out.println();
 		getRecomendaciones(comentarios);
 
-		
 	}
-	
-	
+
 	public static boolean comentar(Usuario usuario, Tema tema, String comentario) {
 		boolean resultado = true;
 
@@ -86,45 +71,40 @@ public class Main {
 			comentarios.add(new Comentario(usuario, tema, comentario));
 		} catch (Exception e) {
 			resultado = false;
-			}
-		System.err.println((resultado)? MENSAJE_INSERCION_OK:MENSAJE_INSERCION_ERROR);
+		}
+		System.err.println((resultado) ? MENSAJE_INSERCION_OK : MENSAJE_INSERCION_ERROR);
 		return resultado;
 	}
-	
+
 	public static void mostrarComentariosOrdenados() {
 		Collections.sort(comentarios);
 		for (Comentario c : comentarios) {
 			System.out.println(c.toString());
-			}
+		}
 	}
-	
-	public static Usuario[] getUsuariosRecomendacion(Comentario comentario) {
-		int cantidadUsuarios = Comentario.getLikes(comentario);
+
+	public static <T> Usuario[] getUsuariosRecomendacion(T contenido) {
+		int cantidadUsuarios = Comentario.getLikes(contenido);
 		boolean hayUsuarios = (cantidadUsuarios != 0);
 		Usuario[] usuariosComentario = null;
 		if (hayUsuarios) {
-			hayUsuarios = false;				//Reinicia la varia para ver ver si los usuarios que hay, coinciden con nuestra lista
-			usuariosComentario =  new Usuario[cantidadUsuarios];
+			hayUsuarios = false; // Reinicia la varia para ver ver si los usuarios que hay, coinciden con nuestra lista
+			usuariosComentario = new Usuario[cantidadUsuarios];
 			String[] nombres = new String[cantidadUsuarios];
 			for (int i = 0; i < cantidadUsuarios; i++) {
-				nombres[i] = Likes.getLikesFor(comentario)[i];	//Recuperamos cada nombre
+				nombres[i] = Likes.getLikesFor(contenido)[i]; // Recuperamos cada nombre
 				for (Usuario usuario : usuarios) {
 					if (usuario.getUser().equals(nombres[i])) {
 						usuariosComentario[i] = usuario;
-//						System.out.println(usuariosComentario[i].getUser());
 						hayUsuarios = true;
 						break;
 					}
 				}
 			}
 		}
-		return (hayUsuarios)? usuariosComentario:null;
+		return (hayUsuarios) ? usuariosComentario : null;
 	}
-	
-//	public static <T> int getLikes(T contenido) {
-//		return Likes.getLikesFor(contenido).length;
-//	}
-	
+
 	public static <T extends Imprimible> void getRecomendaciones(Collection<T> recomendables) {
 		String mensaje = "Recomendaciones (Likes):";
 		System.out.println(mensaje);
@@ -132,8 +112,21 @@ public class Main {
 			System.out.println(textoRecomendacion(c));
 		}
 	}
-	
-	public static <T extends Imprimible> String textoRecomendacion (T contenido) {
-		return (contenido.comentarioImprimible()+ " " + Likes.getLikesFor(contenido).length + "* [" + contenido.getUserToUpper() + "]");  
+
+	public static <T extends Imprimible> String textoRecomendacion(T contenido) {
+		String nombres = "";
+		try {
+			Usuario[] users = getUsuariosRecomendacion(contenido);
+			if (users.length != 0) {
+				for (Usuario usuario : users) {
+					nombres += usuario.getUser() + ", ";
+				}
+				nombres = nombres.substring(0, nombres.length() - 2);
+			}
+		} catch (Throwable e) {
+
+		}
+
+		return (contenido.comentarioImprimible() + " " + Likes.getLikesFor(contenido).length + "* [" + nombres + "]");
 	}
 }
